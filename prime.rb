5e24d7b20f20787ea6f0edd8fcf4dfe38213a1d4
github username <github email address>
2017-08-02 13:09:47 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(integer)
+  #returns a boolean of whether it's a prime
+  #prime number is if a number is divisible only by 1 and itself
+  if integer < 2
+    return false
+  end
+  for counter in 2..(integer - 1) do
+    if integer % counter == 0
+      return false
+    end
+  end
+    return true
+end
